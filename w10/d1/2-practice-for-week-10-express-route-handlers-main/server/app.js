8249a721652843e343ea77844970007ebd4c9817
Alec Keeler <alec.keeler@gmail.com>
2022-10-31 17:45:30 -0500

    d1

--- w10/d1/2-practice-for-week-10-express-route-handlers-main/server/app.js
+++ w10/d1/2-practice-for-week-10-express-route-handlers-main/server/app.js
@@ -25,6 +25,28 @@ const express = require('express');
 const app = express();
 
 // Your code here
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Body:', req.body);
+  next();
+});
+
+// Components of an end point
+// 1. method: get, post, put/patch, delete, use, all
+// 2. path/url: string, regular expression, array of strings/regexes
+// 3. middleware: functions, array of functions
+// 4. response: json, send, redirect, render
+
+
+app.get('/artists', (req, res) => {
+  res.json(getAllArtists())
+})
+
+app.post('/artists', (req, res) => {
+  res.status(201)
+  res.json(addArtist(req.body))
+})
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
