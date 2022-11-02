e42a91e9ac4214e045dabca4bf0dcc6b2b3cdc61
Nathan Treadaway <natetread@gmail.com>
2022-11-02 11:46:34 -0400

    added solution comments for prac 1

--- w10/d3/1-practice-for-week-10-sql-practice-sqlite3-cli-main/sql-practice/example.sql
+++ w10/d3/1-practice-for-week-10-sql-practice-sqlite3-cli-main/sql-practice/example.sql
@@ -3,3 +3,10 @@ DROP TABLE IF EXISTS samples;
 CREATE TABLE samples (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT);
 
 INSERT INTO samples (name) VALUES ('You found me!'), ('Yippee!!'), ('Yay!!!');
+
+
+-- Step 1 .help will show built in docs
+    -- .tables to show the tables
+    -- .schema to show the schema
+--Step 3 .headers on will show the column names
+    -- .mode column will give prettier output that is easer to read
