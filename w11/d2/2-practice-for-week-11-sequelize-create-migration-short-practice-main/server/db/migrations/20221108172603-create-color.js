689970479aec6ad202d76c543e0c5a7253699273
Alec Keeler <alec.keeler@gmail.com>
2022-11-08 13:13:00 -0600

    w11d2 part way

---
+++ w11/d2/2-practice-for-week-11-sequelize-create-migration-short-practice-main/server/db/migrations/20221108172603-create-color.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable("Colors", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      name: {
+        type: Sequelize.STRING(100),
+        unique: true,
+        allowNull: false
+      }
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable('Colors')
+  }
+};
+
+// npx dotenv sequelize db:migrate
+// npx dotenv sequelize db:migrate:undo:all
